fbff9aade509de565b61f7f193bc2e0a60aa94cc
rakeezam <rakeezamalik@gmail.com>
2026-08-08 09:42:06 +0400

    New line for her in about

--- src/app/(genres)/about/page.tsx
+++ src/app/(genres)/about/page.tsx
@@ -23,17 +23,19 @@ export default function AboutPage() {
           <h1 className={styles.name}>About</h1>
           <p className={styles.bio}>
             Maryam Adam is an interdisciplinary artist, illustrator, and
-            designer with a BA in Graphic Communication Design. Her work
-            explores themes of heritage, introspection, and the connection
-            between the conscious and unconscious. Community engagement is
-            central to her practice, and she values the relationships formed
-            with audiences through her concepts. Often incorporating surrealist
-            imagery, her work addresses social politics, faith, and existence.
-            Her practice visually reflects the Gujarati/interlingual word
-            &quot;naram&quot; (soft, gentle), even when addressing more
-            subversive themes. Since winning the Jo Brocklehurst Prize in 2021,
-            she has gone on to work with the likes of Dazed, V&A East, BFI, NHS,
-            and Islamic Relief.
+            designer with a BA in Graphic Communication Design.
+          </p>
+          <p className={styles.bio}>
+            Her work explores themes of heritage, introspection, and the
+            connection between the conscious and unconscious. Community
+            engagement is central to her practice, and she values the
+            relationships formed with audiences through her concepts. Often
+            incorporating surrealist imagery, her work addresses social
+            politics, faith, and existence. Her practice visually reflects
+            the Gujarati/interlingual word &quot;naram&quot; (soft, gentle),
+            even when addressing more subversive themes. Since winning the
+            Jo Brocklehurst Prize in 2021, she has gone on to work with the
+            likes of Dazed, V&A East, BFI, NHS, and Islamic Relief.
           </p>
         </div>
         <div className={styles.articlesPane}>
